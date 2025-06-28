98a3333148402bb19d801f4e959b477cca6f3a30
Stanislav Molchanov <stasmolchanov7@gmail.com>
2025-06-28 14:29:05 +0300

    Added redirection controller

---
+++ Web/Controllers/RedirectController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interfaces.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("{code}")]
+    public class RedirectController : ControllerBase
+    {
+        private readonly IShortUrlRepository _repository;
+
+        public RedirectController(IShortUrlRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RedirectToLongUrl(string code)
+        {
+            var shortUrl = await _repository.GetByCodeAsync(code);
+            if (shortUrl == null) return NotFound("Посилання не знайдено");
+
+            shortUrl.RegisterClick();
+
+            await _repository.UpdateAsync(shortUrl);
+
+            return Redirect(shortUrl.LongUrl);
+        }
+    }
+}
